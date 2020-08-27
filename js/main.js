32c48a7c27a34e22158ab50ab299ef5fa9625bf5
Rivstein <51017849+Rivstein@users.noreply.github.com>
2020-08-27 22:47:03 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,3 +8,16 @@ function toggle_nav_close() {
 	document.getElementById('nav-close').style.display="none";
 	document.getElementById('mobile-nav').style.display="none";
 }
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
